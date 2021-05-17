3fff00337a535d6153d1b7234a2f62ac0154cd02
David Sanders <dasanders@mavs.coloradomesa.edu>
2021-05-17 09:55:24 -0600

    touchups

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # FullStackFinal
+| Name | Value |
+| :--- | :--- |
+| Due Date | 5-17-21 |
+| Status | Done |
+| Location | https://github.com/youand18/FullStackFinal|
+| Notes | Completed deployment of signup functionality. |
 
 ## Connect 4
 Added a signup feature in order to track the number of wins a player obtains.
--- back-end/middleware/auth.js
+++ back-end/middleware/auth.js
@@ -1,15 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  //Get token from Header
+
   const token = req.header("x-auth-token");
 
-  // Check if no token
   if (!token) {
     return res.status(401).json({ msg: "No token auth denied " });
   }
 
-  //Verify the token
   try {
     const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
     req.user = decodedToken.user;
